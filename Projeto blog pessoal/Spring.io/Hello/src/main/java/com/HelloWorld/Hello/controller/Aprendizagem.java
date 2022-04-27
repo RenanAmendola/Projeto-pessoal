0e985c1bb71a43d42c7d81b8025b3bab81fbd110
Renan Amendola <renanamendola@hotmail.com>
2022-04-27 14:45:30 -0300

    Arrumando para a atividade

--- Projeto blog pessoal/Spring.io/Hello/src/main/java/com/HelloWorld/Hello/controller/Aprendizagem.java
+++ Projeto blog pessoal/Spring.io/Hello/src/main/java/com/HelloWorld/Hello/controller/Aprendizagem.java
@@ -1,4 +1,4 @@
-package com.AprendizagemSemanal.Aprendizagem.controller;
+package com.HelloWorld.Hello.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
--- Projeto blog pessoal/Spring.io/Hello/src/main/java/com/HelloWorld/Hello/controller/Lista.java
+++ Projeto blog pessoal/Spring.io/Hello/src/main/java/com/HelloWorld/Hello/controller/Lista.java
@@ -1,4 +1,4 @@
-package com.LIstaBSMs.Lista.controller;
+package com.HelloWorld.Hello.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -6,11 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/ListaBSMs")
-public class ListaBSMs {
+public class Lista {
 
 	
 	@GetMapping	
-	public String ListaBSMs() {
+	public String HelloWorld() {
 		return "Mentalidade de crescimento, Percistencia,"
 				+ "Responsabilidade pessoal, Orientação ao Futuro,"
 				+ "Comunicação, Orientação ao Detalhe, Proatividade eTrabalho em Equipe";
