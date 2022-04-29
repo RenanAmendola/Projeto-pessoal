b8de26b22dbc7c83a0840f29273c9f4718bcc80e
Renan Amendola <renanamendola@hotmail.com>
2022-04-29 12:39:51 -0300

    Adicao de: get titulo, post, put e delet

--- Projeto blog pessoal/Blog spring/blogpessoal/src/main/java/com/blogpessoal/blogpessoal/repository/PostagemRepository.java
+++ Projeto blog pessoal/Blog spring/blogpessoal/src/main/java/com/blogpessoal/blogpessoal/repository/PostagemRepository.java
@@ -9,7 +9,7 @@ import com.blogpessoal.blogpessoal.model.Postagem;
 
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
-	public List<Postagem> findAllByTituloContainingIgnoreCase (String titulo);
+	public List<Postagem>findAllByTituloContainingIgnoreCase(String titulo);
 	
 	
 	
