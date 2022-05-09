d8b17ac47633b966f578a6dc1722e4edaf9cfa10
Renan Amendola <renanamendola@hotmail.com>
2022-05-09 15:14:47 -0300

    adicao do security

--- Projeto blog pessoal/Blog spring/blogpessoal/src/main/java/com/blogpessoal/blogpessoal/model/Postagem.java
+++ Projeto blog pessoal/Blog spring/blogpessoal/src/main/java/com/blogpessoal/blogpessoal/model/Postagem.java
@@ -40,6 +40,21 @@ public class Postagem {
 	@ManyToOne
 	@JsonIgnoreProperties("postagem")
 	private Tema tema;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Usuario usuario;
+
+
+	
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
 
 
 	public Tema getTema() {
---
+++ Projeto blog pessoal/Blog spring/blogpessoal/src/main/java/com/blogpessoal/blogpessoal/model/Usuario.java
@@ -0,0 +1,99 @@
+package com.blogpessoal.blogpessoal.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "TB_usuario")
+public class Usuario {
+
+	
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long id;
+
+@NotNull
+private String nome;
+
+@Size(max = 5000)
+private String foto;
+
+@NotNull
+@Email
+private String usuario;
+
+@NotNull
+@Size(min = 5)
+private String senha;
+
+@OneToMany(mappedBy = "usuario", cascade = CascadeType.REMOVE)
+@JsonIgnoreProperties("usuario")
+private List<Postagem> postagem;
+
+
+
+public Long getId() {
+	return id;
+}
+
+public void setId(Long id) {
+	this.id = id;
+}
+
+public String getNome() {
+	return nome;
+}
+
+public void setNome(String nome) {
+	this.nome = nome;
+}
+
+public String getFoto() {
+	return foto;
+}
+
+public void setFoto(String foto) {
+	this.foto = foto;
+}
+
+public String getUsuario() {
+	return usuario;
+}
+
+public void setUsuario(String usuario) {
+	this.usuario = usuario;
+}
+
+public String getSenha() {
+	return senha;
+}
+
+public void setSenha(String senha) {
+	this.senha = senha;
+}
+
+public List<Postagem> getPostagem() {
+	return postagem;
+}
+
+public void setPostagem(List<Postagem> postagem) {
+	this.postagem = postagem;
+}
+
+
+
+
+
+}
